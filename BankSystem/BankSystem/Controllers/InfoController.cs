4278a5aa9118b8143f9c6eaf4d2b8fb0dc5dc84f
Kiragrom <kiragrom@gmail.com>
2015-11-08 00:38:40 +0300

    Add bootstrap

---
+++ BankSystem/BankSystem/Controllers/InfoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BankSystem.Controllers
+{
+    public class InfoController : Controller
+    {
+      
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Individuals()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Comercial()
+        {
+            return View();
+        }
+
+    }
+}
