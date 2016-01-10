0b8824be1987e2e530b03bcd2b3c7be81740b584
Kiragrom <kiragrom@gmail.com>
2016-01-10 04:28:14 +0300

    Add currency calculator and some css

---
+++ BankSystem/BankSystem/Scripts/calculator.js
@@ -0,0 +1,4 @@
+﻿$('#currency-btn').click(function () {
+    var res = parseInt($('#currency-1').val()) * parseInt($('#currency-1-name').val()) / parseInt($('#currency-2-name').val());
+    $('#currency-2').val(res);
+});
